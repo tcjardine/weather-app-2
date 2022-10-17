9a1f0d422f27ed7a528c244986e2f066086f88ee
tcjardine <112444700+tcjardine@users.noreply.github.com>
2022-10-16 20:27:37 -0700

    Began adding display functions with Open Weather and Axios to current weather data
    Temperature, city, description, humidity, wind

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "88d70de67eac2bcba42225890fa32727";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
