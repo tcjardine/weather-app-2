2e20a2ffb1b419c39ae473ece47a0084968ea19b
tcjardine <112444700+tcjardine@users.noreply.github.com>
2022-10-25 18:37:44 -0700

    Started display forecast for week
    Started array with 4 days (inserting fake data for now)

--- src/app.js
+++ src/app.js
@@ -21,6 +21,34 @@ function formateDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+                    <div class="col-2">
+                      <div class="weather-forecast-date">${day}</div>
+                      <img
+                        src="https://openweathermap.org/img/wn/04d@2x.png"
+                        alt=""
+                        width="42px"
+                      />
+                      <div class="weather-forecast-temp">
+                        <span class="weather-forecast-temp-max">40°</span>
+                        <span class="weather-forecast-temp-min">12°</span>
+                      </div>
+                    </div>
+                  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -61,3 +89,4 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("chicago");
+displayForecast();
