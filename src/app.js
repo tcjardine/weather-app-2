18ecf9c1a654fe4609b7fcd1928c953322bbb725
tcjardine <112444700+tcjardine@users.noreply.github.com>
2022-11-02 18:47:46 -0700

    Added background image and app background color

--- src/app.js
+++ src/app.js
@@ -69,10 +69,9 @@ function displayForecast(response) {
 }
 
 function getForcast(coordinates) {
-  console.log(coordinates);
   let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 function displayTemperature(response) {
