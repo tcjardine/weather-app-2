f0b130ee959f15d35ab1822597f72c1e3c72b325
tcjardine <112444700+tcjardine@users.noreply.github.com>
2022-11-04 10:26:54 -0700

    Updated kilometers to miles conversion for wind speed

--- src/app.js
+++ src/app.js
@@ -88,7 +88,7 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
+  windElement.innerHTML = Math.round(response.data.wind.speed / 0.62137);
   dateElement.innerHTML = formateDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
